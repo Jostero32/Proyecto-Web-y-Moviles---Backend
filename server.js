1895be85f3549695ecad542b623816a6545c458d
Jostero32 <jostero32@gmail.com>
2025-09-22 15:46:10 -0500

    cambio roles predefinidos

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 4000;
     console.log("✅ Tablas sincronizadas");
     let email="email@email.com";
     let roleName="Administrador";
-    await Role.bulkCreate([{id:1, roleName: roleName }, {id:2, roleName: "Comprador" },{id:3,roleName:"Vendedor"}], { ignoreDuplicates: true }); // para que no reviente si ya existen
+    await Role.bulkCreate([{id:1, roleName: roleName }, {id:2, roleName: "Usuario" }], { ignoreDuplicates: true }); // para que no reviente si ya existen
     await User.bulkCreate([{id:1, dni: "00000000", email: email , name: "Admin",lastname:"Admin", passwordHash: "$2b$10$b1c0c5afGkn2LcVcnBLLb.jugmRAc.utHMzNA2fJMhOjWU7lLG/F2", phone: "000000000", avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHfd3PPulVSp4ZbuBFNkePoUR_fLJQe474Ag&s", rating: 5 }
     ], { ignoreDuplicates: true }); 
     let user= await User.findOne({where:{email:email}});
